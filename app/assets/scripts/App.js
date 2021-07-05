5ca4b378aad435673c6f1ef7209c187903cb11f1
omidfa <omidfa1234@gmail.com>
2021-07-05 22:24:12 +0430

    all doen

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -4,22 +4,21 @@ import MobileMenu from './modules/MobileMenu'
 import RevealOnScroll from './modules/RevealOnScroll'
 import StickyHeader from './modules/StickyHeader'
 
-
-let stickyHeader = new StickyHeader()
+new StickyHeader()
 new RevealOnScroll(document.querySelectorAll(".feature-item"), 75)
 new RevealOnScroll(document.querySelectorAll(".testimonial"), 60)
-let mobileMenu = new MobileMenu();
+new MobileMenu();
 let modal
 
 document.querySelectorAll(".open-modal").forEach(el => {
-  el.addEventListener("click", e=> {
+  el.addEventListener("click", e => {
     e.preventDefault()
-    if(typeof modal == "undefined") {
-      import(/*webpackChunkName:"modal" */'./modules/Modal').then(x => {
-        modal= new x.default()
-        setTimeout(() => modal.openTheModal(), 20 )
-    }).catch(() => console.log("there was a problem."))
-    }else {
+    if (typeof modal == "undefined") {
+      import(/* webpackChunkName: "modal" */ './modules/Modal').then(x => {
+        modal = new x.default()
+        setTimeout(() => modal.openTheModal(), 20)
+      }).catch(() => console.log("There was a problem."))
+    } else {
       modal.openTheModal()
     }
   })
--- app/assets/scripts/modules/Modal.js
+++ app/assets/scripts/modules/Modal.js
@@ -7,7 +7,6 @@ class Modal {
   }
 
   events() {
-
     // listen for close click
     this.closeIcon.addEventListener("click", () => this.closeTheModal())
 
@@ -33,9 +32,9 @@ class Modal {
     document.body.insertAdjacentHTML('beforeend', `
     <div class="modal">
     <div class="modal__inner">
-      <h2 class="section-title section-title--blue section-title--less-margin"><img src="assets/images/icons/mail.svg" class="section-title__icon">  <strong>تماس</strong>  با ما</h2>
+      <h2 class="section-title section-title--blue section-title--less-margin"><img src="assets/images/icons/mail.svg" class="section-title__icon"> Get in <strong>Touch</strong></h2>
       <div class="wrapper wrapper--narrow">
-        <p class="modal__description">سفارش انلاین ما به زودی راه اندازی میشود.تا ان زمان میتوانید در یکی از پلتفرم های زیر با ما در ارتباط باشید</p>
+        <p class="modal__description">We will have an online order system in place soon. Until then, connect with us on any of the platforms below!</p>
       </div>
 
       <div class="social-icons">
